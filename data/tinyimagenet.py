ae1e2646ad0b15e32e9660a181f20687aac839ca
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-05-20 17:31:35 +0900

    Update tinyimagenet.py

--- data/tinyimagenet.py
+++ data/tinyimagenet.py
@@ -7,6 +7,7 @@ def get_tinyimagenet_loaders(root="./data/tinyimagenet", batch_size=128, num_wor
     """
     Returns:
         train_loader, test_loader
+        TinyImageNet size (64×64)
     """
     transform_train = T.Compose([
         T.Resize((64,64)),
