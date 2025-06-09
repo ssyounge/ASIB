dd461edd5b7e4c16c8570c530294650038e066ca
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-06-09 23:19:37 +0900

    Add LightweightAttnMBM and config support

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
+from .mbm import ManifoldBridgingModule, SynergyHead, build_from_teachers
+from .la_mbm import LightweightAttnMBM
 
+__all__ = [
+    "ManifoldBridgingModule",
+    "SynergyHead",
+    "LightweightAttnMBM",
+    "build_from_teachers",
+]
