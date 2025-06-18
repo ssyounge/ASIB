af90991388ebf47b02b18cd3a60e95590342d8d3
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-06-18 17:50:28 +0900

    Allow generate_config to merge multiple YAML files

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -1,15 +1,34 @@
 # scripts/generate_config.py
 
-import argparse, yaml
+import argparse
+import glob
+import os
+import yaml
 
 parser = argparse.ArgumentParser(description="Merge base YAML with overrides")
-parser.add_argument('--base', required=True, help='Base YAML file')
+parser.add_argument(
+    "--base",
+    required=True,
+    nargs="+",
+    help="One or more YAML files or a directory of YAML fragments",
+)
 parser.add_argument('--out', required=True, help='Output YAML file')
 parser.add_argument('overrides', nargs='*', help='KEY=VAL pairs to override')
 args = parser.parse_args()
 
-with open(args.base) as f:
-    cfg = yaml.safe_load(f)
+cfg = {}
+for base in args.base:
+    paths = []
+    if os.path.isdir(base):
+        paths.extend(sorted(glob.glob(os.path.join(base, "*.yml"))))
+        paths.extend(sorted(glob.glob(os.path.join(base, "*.yaml"))))
+    else:
+        paths.append(base)
+    for p in paths:
+        with open(p) as f:
+            data = yaml.safe_load(f)
+            if data:
+                cfg.update(data)
 
 for ov in args.overrides:
     if '=' not in ov:
