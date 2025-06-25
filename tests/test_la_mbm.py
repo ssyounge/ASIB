2c59284f5e1490af1583831d6b8339af4ca21384
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-06-25 16:16:20 +0900

    Add latent space alignment outputs

--- tests/test_la_mbm.py
+++ tests/test_la_mbm.py
@@ -10,7 +10,7 @@ def test_custom_query_dim_forward():
                               learnable_q=False, query_dim=8)
     q = torch.randn(4, 8)
     feats = [torch.randn(4, 16), torch.randn(4, 16)]
-    out, attn = mbm(q, feats)
+    out, attn, q_proj, attn_out = mbm(q, feats)
     assert out.shape == (4, 32)
     assert attn.shape[0] == 4
 
@@ -20,7 +20,7 @@ def test_zero_query_dim_uses_teacher_dim():
                               learnable_q=False, query_dim=0)
     q = torch.randn(2, 32)  # sum of teacher dims = 32
     feats = [torch.randn(2, 16), torch.randn(2, 16)]
-    out, attn = mbm(q, feats)
+    out, attn, q_proj, attn_out = mbm(q, feats)
     assert out.shape == (2, 32)
     assert attn.shape[0] == 2
 
