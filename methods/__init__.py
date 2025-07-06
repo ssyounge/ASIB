c3e910fcd52a2430d98c0c789074a1ab1dd0e2c5
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-07 00:14:46 +0900

    Add feature-level KD support

--- methods/__init__.py
+++ methods/__init__.py
@@ -2,9 +2,11 @@
 from .crd import CRDDistiller
 from .dkd import DKDDistiller
 from .vanilla_kd import VanillaKDDistiller
+from .feature_kd import FeatureKD
 
 __all__ = [
     "CRDDistiller",
     "DKDDistiller",
     "VanillaKDDistiller",
+    "FeatureKD",
 ]
