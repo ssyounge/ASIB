f0d5e015419dea559a2eb5beca6aba8cf975e294
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-07 12:26:37 +0900

    Remove KD branch and update MBM

--- methods/__init__.py
+++ methods/__init__.py
@@ -2,11 +2,9 @@
 from .crd import CRDDistiller
 from .dkd import DKDDistiller
 from .vanilla_kd import VanillaKDDistiller
-from .feature_kd import FeatureKD
 
 __all__ = [
     "CRDDistiller",
     "DKDDistiller",
     "VanillaKDDistiller",
-    "FeatureKD",
 ]
