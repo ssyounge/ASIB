e9463065cfbd999d21dabc05fc1cc8738e5fe0a6
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-07 23:43:02 +0900

    Expose additional distillers

--- methods/__init__.py
+++ methods/__init__.py
@@ -2,9 +2,13 @@
 from .crd import CRDDistiller
 from .dkd import DKDDistiller
 from .vanilla_kd import VanillaKDDistiller
+from .at import ATDistiller
+from .fitnet import FitNetDistiller
 
 __all__ = [
     "CRDDistiller",
     "DKDDistiller",
     "VanillaKDDistiller",
+    "ATDistiller",
+    "FitNetDistiller",
 ]
