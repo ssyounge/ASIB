21d631454efbdb2523ea4836b7e083b94a1b04a6
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-15 11:43:40 +0900

    Update make_snapshots.sh

--- scripts/make_snapshots.sh
+++ scripts/make_snapshots.sh
@@ -1,5 +1,6 @@
 # scripts/make_snapshots.sh
-#!/bin/bash
+
+#!/bin/bash -l                 # ✅ 첫 줄에 바로 shebang
 #SBATCH --job-name=snap_res152
 #SBATCH --partition=base_suma_rtx3090
 #SBATCH --gres=gpu:1
