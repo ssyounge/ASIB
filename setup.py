faceeb145308eed7dce4196727ad9afba5ba26c4
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-26 18:25:40 +0900

    Filter comments in install_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="asib_kd",
     version="0.2.0",
     packages=find_packages(),
-    install_requires=[line.strip() for line in open("requirements.txt")],
+    install_requires=[
+        ln.strip()
+        for ln in open("requirements.txt")
+        if ln.strip() and not ln.startswith("#")
+    ],
     python_requires=">=3.8",
 )
