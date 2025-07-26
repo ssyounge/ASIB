0dd40ec849cb9efbcf5b44bd5636295fc88e8a16
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-26 20:20:56 +0900

    fix setup requirements path

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+
+req_path = Path(__file__).resolve().parent / "requirements.txt"
+with req_path.open() as f:
+    install_requires = [
+        ln.strip()
+        for ln in f
+        if ln.strip() and not ln.startswith("#")
+    ]
 
 setup(
     name="asib_kd",
     version="0.2.0",
     packages=find_packages(),
-    install_requires=[
-        ln.strip()
-        for ln in open("requirements.txt")
-        if ln.strip() and not ln.startswith("#")
-    ],
+    install_requires=install_requires,
     python_requires=">=3.8",
 )
