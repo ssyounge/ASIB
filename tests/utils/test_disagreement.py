ab941b3e390aa5e80e2de5a3047a237e8b8070ed
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-26 17:47:46 +0900

    Delete tests directory

--- tests/utils/test_disagreement.py
+++
@@ -1,9 +0,0 @@
-import torch
-from modules.disagreement import compute_disagreement_rate
-
-
-def test_disagreement_rate_range(dummy_teachers):
-    t1, t2 = dummy_teachers
-    loader = [(torch.zeros(1, 3), torch.tensor([0]))]
-    rate = compute_disagreement_rate(t1, t2, loader, device="cpu")
-    assert 0.0 <= rate <= 100.0
