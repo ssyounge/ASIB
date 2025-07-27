7ad91a7d542598d1131e04214b7fcf686e03d61f
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-28 08:29:40 +0900

    Update run_finetune_clean.sh

--- run_finetune_clean.sh
+++ run_finetune_clean.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=01:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=logs/ft_%j.log
 #SBATCH --error=logs/ft_%j.err
 # ---------------------------------------------------------
