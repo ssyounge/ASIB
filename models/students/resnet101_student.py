dfdd2fe48357db0186b9e762268bad0edaca2d7f
Suyoung Yang <99642470+ssyounge@users.noreply.github.com>
2025-07-30 08:55:02 +0900

    Update resnet101_student.py

--- models/students/resnet101_student.py
+++ models/students/resnet101_student.py
@@ -10,7 +10,6 @@ from models.common.adapter import ChannelAdapter2D
 
 @register("resnet101_pretrain_student")
 @register("resnet101_scratch_student")
-
 class ResNetStudent(BaseKDModel):
     def __init__(self, *, pretrained: bool = True, num_classes: int = 100,
                  small_input: bool = False, cfg: dict | None = None):
